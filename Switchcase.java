08f484b0a1763b95addf38c227e8693971ddc36d
NataliaBobo <nataliawanjikubobo@gmail.com>
2024-07-09 09:04:44 +0300

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,23 @@
+public class Switchcase{
+  public static void main(String[]args){
+
+  char grade=A;
+Switch(grade);
+{
+ case A: System.out.println("Excellent. keep it up");
+         break;
+ case B: System.out.println("Good work"); 
+         break;
+ case C: System.out.println("Work hard"); 
+         break;
+ case D: System.out.println("Room for improvement"); 
+         break;
+ case E: System.out.println("Pull up your socks");
+         break;
+ default: System.out.println("unknown");
+         break;
+
+}
+ 
+}
+}
